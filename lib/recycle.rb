8ecf05dab263fe3b3423c3483934a73250382716
hgrobel <hmgrobel@gmail.com>
2018-07-12 18:19:06 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -32,7 +32,7 @@ end
 #This method should print out each key in the hash. 
 #You should use the `each_key` method to iterate over your hash and print out each key.
 def all_keys(first_recycle_bin)
-  puts first_recycle_bin.each_key 
+  puts first_recycle_bin.keys
 end
 
 
